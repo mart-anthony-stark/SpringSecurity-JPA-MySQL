1d1bc8023de918a7412b7312c1680f7bf5ba30c8
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-11 20:56:58 +0800

    Add: home controller

---
+++ src/main/java/io/anthony/security/HomeResource.java
@@ -0,0 +1,22 @@
+package io.anthony.security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+	@GetMapping("/")
+	public String home() {
+		return ("<h1>Welcome Home</h1>");
+	}
+	
+	@GetMapping("/user")
+	public String user() {
+		return ("<h1>Welcome User</h1>");
+	}
+	
+	@GetMapping("/admin")
+	public String admin() {
+		return ("<h1>Welcome Admin</h1>");
+	}
+}
