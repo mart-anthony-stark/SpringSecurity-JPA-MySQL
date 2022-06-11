3045bd566e57e2f67998132c52bcc443a35ba87a
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-11 21:55:38 +0800

    Add: user details service

--- src/main/java/io/anthony/security/MyUserDetailsService.java
+++ src/main/java/io/anthony/security/MyUserDetailsService.java
@@ -1,17 +1,30 @@
 package io.anthony.security;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import io.anthony.security.models.MyUserDetails;
+import io.anthony.security.models.User;
+
 @Service
 public class MyUserDetailsService implements UserDetailsService {
+
+	@Autowired
+    UserRepository userRepository;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		// TODO Auto-generated method stub
-		return new MyUserDetails(username);
+		Optional<User> user = userRepository.findByUserName(username);
+		
+		user.orElseThrow(() -> new UsernameNotFoundException("Not found: "+ username));
+		
+		return user.map(MyUserDetails::new).get();
 	}
 
 }
