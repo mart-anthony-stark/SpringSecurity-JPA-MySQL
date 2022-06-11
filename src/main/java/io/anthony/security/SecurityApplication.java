9498c99e0e8674cda7a8b7d3103a74a7ec498eba
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-11 21:55:55 +0800

    Add: base package classes for jpa repo

--- src/main/java/io/anthony/security/SecurityApplication.java
+++ src/main/java/io/anthony/security/SecurityApplication.java
@@ -2,8 +2,10 @@ package io.anthony.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class SecurityApplication {
 
 	public static void main(String[] args) {
