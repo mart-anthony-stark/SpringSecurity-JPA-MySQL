ee50f6e8d2456019c74250442efd64136714e5f4
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-11 20:05:24 +0800

    First commit

---
+++ src/test/java/io/anthony/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.anthony.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
