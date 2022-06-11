ad9e6cb8da6309ff332904c0e71c678943879a4a
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-11 21:56:13 +0800

    Add: user details

--- src/main/java/io/anthony/security/models/MyUserDetails.java
+++ src/main/java/io/anthony/security/models/MyUserDetails.java
@@ -1,7 +1,9 @@
-package io.anthony.security;
+package io.anthony.security.models;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -10,25 +12,29 @@ import org.springframework.security.core.userdetails.UserDetails;
 public class MyUserDetails implements UserDetails {
 
 	private String userName;
+	private String password;
+	private boolean active;
+	private List<GrantedAuthority> authorities;
 	
-	public MyUserDetails(String userName) {
-		this.userName = userName;
+	public MyUserDetails(User user) {
+		this.userName = user.getUserName();
+		this.password = user.getPassword();
+		this.active = user.isActive();
+		this.authorities = Arrays.stream(user.getRoles().split(","))
+							.map(SimpleGrantedAuthority::new)
+							.collect(Collectors.toList());
 	}
-	
-	public MyUserDetails() {
-		
-	}
-	
+
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		// TODO Auto-generated method stub
-		return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+		return authorities;
 	}
 
 	@Override
 	public String getPassword() {
 		// TODO Auto-generated method stub
-		return "pass";
+		return password;
 	}
 
 	@Override
@@ -58,7 +64,6 @@ public class MyUserDetails implements UserDetails {
 	@Override
 	public boolean isEnabled() {
 		// TODO Auto-generated method stub
-		return true;
+		return active;
 	}
-
 }
