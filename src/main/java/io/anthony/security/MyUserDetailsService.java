75875693036a2b898741579f41572697173c2292
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-11 20:57:16 +0800

    Add: user details service

---
+++ src/main/java/io/anthony/security/MyUserDetailsService.java
@@ -0,0 +1,17 @@
+package io.anthony.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		return new MyUserDetails(username);
+	}
+
+}
